1c3e4710f6b99a8470f6a69fe53d8d85678a8094
felipefriske <116766407+felipefriske@users.noreply.github.com>
2022-10-27 19:05:38 -0300

    Update pasta teste.php

--- pasta teste.php
+++ pasta teste.php
@@ -2,7 +2,13 @@
 
       echo "tentando exercicio de git hub, será que consegui?";
 
-echo "consegui sim, sou foda, vou tentar atualizar"
+echo "consegui sim, sou foda, vou tentar atualizar";
+      
+      
+      
+      
+      
+      echo"testando a interferencia, será q deu certo?";
 
            
 ?>
