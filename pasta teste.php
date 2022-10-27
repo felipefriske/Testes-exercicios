0b01071d95953270f2d33e4ca02270c056031183
rafaelfriske <felipe.friske@gmail.com>
2022-10-27 19:12:46 -0300

    exercicio de merge git

--- pasta teste.php
+++ pasta teste.php
@@ -2,7 +2,8 @@
 
       echo "tentando exercicio de git hub, será que consegui?";
 
-echo "consegui sim, sou foda, vou tentar atualizar"
+echo "consegui sim, sou foda, vou tentar atualizar";
 
+echo "ahhhh consegui, agora vou criar uma falha de versão";
            
 ?>
